9475c9ff516e54edae207b1dd45a6df89c01a331
JGRex-Joy <omurkulovamir2008@gmail.com>
2025-09-20 18:45:17 +0600

    added Flask

--- app/main.py
+++ app/main.py
@@ -1,21 +1,27 @@
-from fastapi import FastAPI
+import traceback
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from services.embedder import embed_texts
-from services.vector_search import search
-from services.gemini_client import ask_gemini
+from app.services.embedder import embed_texts
+from app.services.vector_search import search
+from app.services.gemini_client import ask_gemini
 
-FAISS_FILE = "embeddings/index.faiss"
-META_FILE = "embeddings/metadata.json"
+FAISS_FILE = "app/embeddings/index.faiss"
+META_FILE = "app/embeddings/metadata.json"
 
-app = FastAPI()
+app = FastAPI(title="Startup Nation API", version="1.0")
 
 class Query(BaseModel):
     question: str
 
 @app.post("/ask")
 def ask(query: Query):
-    query_emb = embed_texts([query.question])
-    results = search(query_emb, FAISS_FILE, META_FILE, top_k=3)
-    context = "\n".join([r["text"] for r in results])
-    answer = ask_gemini(query.question, context)
-    return {"answer": answer, "chunks": results}
+    try:
+        query_emb = embed_texts([query.question])
+        results = search(query_emb, FAISS_FILE, META_FILE, top_k=15)
+        context = "\n".join([r["text"] for r in results])
+        answer = ask_gemini(query.question, context)
+        return {"answer": answer}
+    except Exception as e:
+        print("Ошибка в /ask:")
+        traceback.print_exc()  
+        raise HTTPException(status_code=500, detail=str(e))
